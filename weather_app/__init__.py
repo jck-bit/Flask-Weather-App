4337079d98c0fed8896323c01e6b3d56936fca16
jck-bit <jckkinyanjui@gmail.com>
2022-09-24 23:18:54 +0300

    requests can be made to the api

--- weather_app/__init__.py
+++ weather_app/__init__.py
@@ -1,17 +1,18 @@
 import requests
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-
+3
 app = Flask(__name__)
+db = SQLAlchemy()
 
-City_API_endpoint = "http://api.openweathermap.org/data/2.5/weather?q="
-City = "Nairobi"
-Country = ",KE,"
-join_key = "&appid=" + "eb33b9299fdaf865b24df40ad97c627e"
-units = "&units=metric"
 
-current_city_weather = City_API_endpoint + City + Country + join_key + units
-print(current_city_weather)
+url = "http://api.openweathermap.org/data/2.5/weather?q={}&units+imperial&&appid=eb33b9299fdaf865b24df40ad97c627e"
+city = "Nairobi"
+
+
+
+r = requests.get(url.format(city)).json()
+print(r)
 
 @app.route('/')
 def index():
