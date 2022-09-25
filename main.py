a0934a8a39167a27d3891db16fc20dbaac1ab7ba
jck-bit <jckkinyanjui@gmail.com>
2022-09-25 18:52:39 +0300

    Made my app into a package

--- main.py
+++ main.py
@@ -1,41 +1,4 @@
-import requests
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-
-
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
-
-db = SQLAlchemy(app)
-
-
-
-class City(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-
-@app.route('/')
-def index():
-
-  url = "http://api.openweathermap.org/data/2.5/weather?q={}&units+imperial&&appid=eb33b9299fdaf865b24df40ad97c627e"
-  city = "Nairobi"
-
-
-  r = requests.get(url.format(city)).json()
-
-  weather = {
-    'city' : city ,
-    'temperature' : r['main']['temp'],
-    'description' : r['weather'][0]['description'],
-    'icon': r['weather'][0]['icon'],
-   }
-
-  print(weather) 
-
-  return render_template('weather.html', weather=weather)
-
-
+from weather_app import app
 
 if __name__ == '__main__':
     app.run(debug=True)
